1fbd520daa77ce50fcc5ef374d6036b6905f8de5
hudakjakubdesign <hudakjakubdesign@gmail.com>
2026-07-19 18:44:35 +0100

    Load landscape experience styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./experience.css";
 
 const inter = Inter({
   variable: "--font-inter",
